29a9bbca4ad628b2c82111c4f32d0e9fcd210cc8
Administrator <Administrator@smart000>
2018-07-17 17:30:57 +0900

    [김진우] 1.connection연결 클래스 작성 2.bookDao,bookDto 규칙대로 작성

--- bookshop_model1_service/src/dao/BookDAO.java
+++ bookshop_model1_service/src/dao/BookDAO.java
@@ -1,5 +1,31 @@
 package dao;
 
-public class BookDAO {
+import java.sql.*;
+import dbConnection.DbConnection;;
 
+public class BookDAO {
+	private Connection connection = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	
+	//책 추가 메서드
+	//리턴 0:실패, 1:성공
+	public int insertBook() {
+		
+		//리턴값을 담을 변수
+		int check=0;
+		
+		//쿼리 실행 문장
+		String sql = "";
+		
+		try {
+			connection = DbConnection.getConnetion();
+			preparedStatement = connection.prepareStatement(sql);
+		}catch (Exception e) {
+			// TODO: handle exception
+		}finally {
+			
+		}
+		return check;
+	}
 }
