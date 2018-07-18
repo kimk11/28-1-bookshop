960e79d3276affb6e6ba3aaab5aa8e7b2f6c597a
Administrator <Administrator@smart000>
2018-07-18 09:55:50 +0900

    [김진우] 1.bookpublisehr dao,dto 생성

---
+++ bookshop_model1_service/src/dto/BookPublisherDTO.java
@@ -0,0 +1,27 @@
+package dto;
+
+public class BookPublisherDTO {
+	private int publisherNo;
+	private String publiserName;
+	private String publisherWebsite;
+	
+	public int getPublisherNo() {
+		return publisherNo;
+	}
+	public void setPublisherNo(int publisherNo) {
+		this.publisherNo = publisherNo;
+	}
+	public String getPubliserName() {
+		return publiserName;
+	}
+	public void setPubliserName(String publiserName) {
+		this.publiserName = publiserName;
+	}
+	public String getPublisherWebsite() {
+		return publisherWebsite;
+	}
+	public void setPublisherWebsite(String publisherWebsite) {
+		this.publisherWebsite = publisherWebsite;
+	}
+	
+}
